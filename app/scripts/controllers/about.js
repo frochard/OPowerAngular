24b4a445dceb8715127d5d1ed52d88069646953f
frochard <user1@148.60.14.89>
2016-03-08 09:21:19 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opowerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the opowerApp
+ */
+angular.module('opowerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name opowerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the opowerApp
+ */
+angular.module('opowerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
